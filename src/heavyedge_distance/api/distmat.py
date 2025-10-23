0404eb0e9deef0e47608435ad8f3d77e7c8f9a1c
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-10-23 17:11:23 +0900

    feat: make template for distmat

--- src/heavyedge_distance/api/distmat.py
+++ src/heavyedge_distance/api/distmat.py
@@ -11,6 +11,110 @@ __all__ = [
 ]
 
 
+def _distmat(
+    converter, distfunc, f1, f2=None, batch_size=None, n_jobs=None, logger=None
+):
+    if n_jobs is not None:
+        pass
+    else:
+        n_jobs = os.environ.get("HEAVYEDGE_MAX_WORKERS")
+        if n_jobs is not None:
+            n_jobs = int(n_jobs)
+        else:
+            n_jobs = 1
+
+    if logger is None:
+        # dummy logger
+        def logger(msg):
+            pass
+
+    x1 = f1.x()
+    if f2 is None:
+        x2 = x1
+    else:
+        x2 = f2.x()
+
+    if f2 is not None:
+        if batch_size is None:
+            fs1, Ls1, _ = f1[:]
+            fs2, Ls2, _ = f2[:]
+            val1 = converter(x1, fs1, Ls1)
+            val2 = converter(x2, fs2, Ls2)
+            D = distfunc(val1, val2, n_jobs)
+            logger("1/1")
+        else:
+            N1, N2 = len(f1), len(f2) if f2 is not None else len(f1)
+            num_batches_1 = (N1 // batch_size) + int(bool(N1 % batch_size))
+            num_batches_2 = (N2 // batch_size) + int(bool(N2 % batch_size))
+
+            D = np.empty((N1, N2), dtype=np.float64)
+
+            for i in range(num_batches_1):
+                fs1, Ls1, _ = f1[i * batch_size : (i + 1) * batch_size]
+                val1 = converter(x1, fs1, Ls1)
+                for j in range(num_batches_2):
+                    fs2, Ls2, _ = f2[j * batch_size : (j + 1) * batch_size]
+                    val2 = converter(x2, fs2, Ls2)
+                    D[
+                        i * batch_size : (i + 1) * batch_size,
+                        j * batch_size : (j + 1) * batch_size,
+                    ] = distfunc(val1, val2, n_jobs)
+                    logger(
+                        f"{i * num_batches_2 + j + 1}/{num_batches_1 * num_batches_2}"
+                    )
+    else:
+        if batch_size is None:
+            fs1, Ls1, _ = f1[:]
+            val1 = converter(x1, fs1, Ls1)
+            D = distfunc(val1, None, n_jobs)
+            logger("1/1")
+        else:
+            N = len(f1)
+            num_batches = (N // batch_size) + int(bool(N % batch_size))
+
+            D = np.empty((N, N), dtype=np.float64)
+
+            for i in range(num_batches):
+                fs1, Ls1, _ = f1[i * batch_size : (i + 1) * batch_size]
+                val1 = converter(x1, fs1, Ls1)
+                # diagonal
+                D[
+                    i * batch_size : (i + 1) * batch_size,
+                    i * batch_size : (i + 1) * batch_size,
+                ] = distfunc(val1, None, n_jobs)
+                # off-diagonal
+                for j in range(i + 1, num_batches):
+                    fs2, Ls2, _ = f1[j * batch_size : (j + 1) * batch_size]
+                    val2 = converter(x2, fs2, Ls2)
+                    dist = distfunc(val1, val2, n_jobs)
+                    D[
+                        i * batch_size : (i + 1) * batch_size,
+                        j * batch_size : (j + 1) * batch_size,
+                    ] = dist
+                    D[
+                        j * batch_size : (j + 1) * batch_size,
+                        i * batch_size : (i + 1) * batch_size,
+                    ] = dist.T
+                    logger(f"{i * num_batches + j + 1}/{num_batches ** 2}")
+    return D
+
+
+def _wasserstein_converter(t):
+    def converter(x, fs, Ls):
+        Qs = quantile(x, fs, Ls, t)
+        return Qs
+
+    return converter
+
+
+def _wasserstein_distfunc(t):
+    def distfunc(Qs1, Qs2, n_jobs):
+        D = wdist(t, Qs1, Qs2)
+        return D
+
+    return distfunc
+
+
 def distmat_wasserstein(t, f1, f2=None, batch_size=None, logger=None):
     """Wasserstein distance matrix between area-scaled profiles.
 
@@ -39,76 +143,17 @@ def distmat_wasserstein(t, f1, f2=None, batch_size=None, logger=None):
     -----
     ``distmat_wasserstein(f1)`` is faster than ``distmat_wasserstein(f1, f1)``.
     """
-    if logger is None:
-        # dummy logger
-        def logger(msg):
-            pass
+    converter = _wasserstein_converter(t)
+    distfunc = _wasserstein_distfunc(t)
+    return _distmat(converter, distfunc, f1, f2, batch_size, logger)
 
-    if f2 is not None:
-        # distmat between f1 and f2
-        if batch_size is None:
-            fs1, Ls1, _ = f1[:]
-            Qs1 = quantile(f1.x(), fs1, Ls1, t)
-            fs2, Ls2, _ = f2[:]
-            Qs2 = quantile(f2.x(), fs2, Ls2, t)
-            D = wdist(t, Qs1, Qs2)
-            logger("1/1")
-        else:
-            N1, N2 = len(f1), len(f2) if f2 is not None else len(f1)
-            num_batches_1 = (N1 // batch_size) + int(bool(N1 % batch_size))
-            num_batches_2 = (N2 // batch_size) + int(bool(N2 % batch_size))
 
-            D = np.empty((N1, N2), dtype=np.float64)
+def _dfd_converter(x, fs, Ls):
+    return (fs, Ls)
 
-            for i in range(num_batches_1):
-                fs1, Ls1, _ = f1[i * batch_size : (i + 1) * batch_size]
-                Qs1 = quantile(f1.x(), fs1, Ls1, t)
-                for j in range(num_batches_2):
-                    fs2, Ls2, _ = f2[j * batch_size : (j + 1) * batch_size]
-                    Qs2 = quantile(f2.x(), fs2, Ls2, t)
-                    D[
-                        i * batch_size : (i + 1) * batch_size,
-                        j * batch_size : (j + 1) * batch_size,
-                    ] = wdist(t, Qs1, Qs2)
-                    logger(
-                        f"{i * num_batches_2 + j + 1}/{num_batches_1 * num_batches_2}"
-                    )
-    else:
-        # distmat of f1 to itself
-        if batch_size is None:
-            fs1, Ls1, _ = f1[:]
-            Qs1 = quantile(f1.x(), fs1, Ls1, t)
-            D = wdist(t, Qs1, None)
-            logger("1/1")
-        else:
-            N = len(f1)
-            num_batches = (N // batch_size) + int(bool(N % batch_size))
 
-            D = np.empty((N, N), dtype=np.float64)
-
-            for i in range(num_batches):
-                fs1, Ls1, _ = f1[i * batch_size : (i + 1) * batch_size]
-                Qs1 = quantile(f1.x(), fs1, Ls1, t)
-                # diagonal
-                D[
-                    i * batch_size : (i + 1) * batch_size,
-                    i * batch_size : (i + 1) * batch_size,
-                ] = wdist(t, Qs1, None)
-                # off-diagonal
-                for j in range(i + 1, num_batches):
-                    fs2, Ls2, _ = f1[j * batch_size : (j + 1) * batch_size]
-                    Qs2 = quantile(f2.x(), fs2, Ls2, t)
-                    dist = wdist(t, Qs1, Qs2)
-                    D[
-                        i * batch_size : (i + 1) * batch_size,
-                        j * batch_size : (j + 1) * batch_size,
-                    ] = dist
-                    D[
-                        j * batch_size : (j + 1) * batch_size,
-                        i * batch_size : (i + 1) * batch_size,
-                    ] = dist.T
-                    logger(f"{i * num_batches + j + 1}/{num_batches ** 2}")
-    return D
+def _dfd_distfunc(val1, val2, n_jobs):
+    return dfd(val1, val2, n_jobs)
 
 
 def distmat_frechet(f1, f2=None, batch_size=None, n_jobs=None, logger=None):
@@ -147,75 +192,4 @@ def distmat_frechet(f1, f2=None, batch_size=None, n_jobs=None, logger=None):
     >>> with ProfileData(get_sample_path("Prep-Type2.h5")) as data:
     ...     D1 = distmat_frechet(data)
     """
-    if n_jobs is not None:
-        pass
-    else:
-        n_jobs = os.environ.get("HEAVYEDGE_MAX_WORKERS")
-        if n_jobs is not None:
-            n_jobs = int(n_jobs)
-        else:
-            n_jobs = 1
-
-    if logger is None:
-        # dummy logger
-        def logger(msg):
-            pass
-
-    if f2 is not None:
-        # distmat between f1 and f2
-        if batch_size is None:
-            Ys1, Ls1, _ = f1[:]
-            Ys2, Ls2, _ = f2[:]
-            D = dfd((Ys1, Ls1), (Ys2, Ls2), n_jobs)
-            logger("1/1")
-        else:
-            N1, N2 = len(f1), len(f2) if f2 is not None else len(f1)
-            num_batches_1 = (N1 // batch_size) + int(bool(N1 % batch_size))
-            num_batches_2 = (N2 // batch_size) + int(bool(N2 % batch_size))
-
-            D = np.empty((N1, N2), dtype=np.float64)
-
-            for i in range(num_batches_1):
-                Ys1, Ls1, _ = f1[i * batch_size : (i + 1) * batch_size]
-                for j in range(num_batches_2):
-                    Ys2, Ls2, _ = f2[j * batch_size : (j + 1) * batch_size]
-                    D[
-                        i * batch_size : (i + 1) * batch_size,
-                        j * batch_size : (j + 1) * batch_size,
-                    ] = dfd((Ys1, Ls1), (Ys2, Ls2), n_jobs)
-                    logger(
-                        f"{i * num_batches_2 + j + 1}/{num_batches_1 * num_batches_2}"
-                    )
-    else:
-        # distmat of f1 to itself
-        if batch_size is None:
-            Ys1, Ls1, _ = f1[:]
-            D = dfd((Ys1, Ls1), None, n_jobs)
-            logger("1/1")
-        else:
-            N = len(f1)
-            num_batches = (N // batch_size) + int(bool(N % batch_size))
-
-            D = np.empty((N, N), dtype=np.float64)
-
-            for i in range(num_batches):
-                Ys1, Ls1, _ = f1[i * batch_size : (i + 1) * batch_size]
-                # diagonal
-                D[
-                    i * batch_size : (i + 1) * batch_size,
-                    i * batch_size : (i + 1) * batch_size,
-                ] = dfd((Ys1, Ls1), None, n_jobs)
-                # off-diagonal
-                for j in range(i + 1, num_batches):
-                    Ys2, Ls2, _ = f1[j * batch_size : (j + 1) * batch_size]
-                    dist = dfd((Ys1, Ls1), (Ys2, Ls2), n_jobs)
-                    D[
-                        i * batch_size : (i + 1) * batch_size,
-                        j * batch_size : (j + 1) * batch_size,
-                    ] = dist
-                    D[
-                        j * batch_size : (j + 1) * batch_size,
-                        i * batch_size : (i + 1) * batch_size,
-                    ] = dist.T
-                    logger(f"{i * num_batches + j + 1}/{num_batches ** 2}")
-    return D
+    return _distmat(_dfd_converter, _dfd_distfunc, f1, f2, batch_size, n_jobs, logger)
